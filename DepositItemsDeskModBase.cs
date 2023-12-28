b3287bd0f5c016fa22347fe61eba2f441c4cdc14
hangyalpeter <hangyal.peter@gmail.com>
2023-12-29 00:13:43 +0100

    release

---
+++ DepositItemsDeskModBase.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using SetCompanySellTime.Patches;
+
+namespace SetCompanySellTime
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class DepositItemsDeskModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "com.peter.lethalcompany.deposititemsdeskmod";
+        private const string modName = "Set Company Sell Time";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static DepositItemsDeskModBase instance;
+
+        internal ManualLogSource log;
+
+        public static ConfigEntry<float> configSellTime;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            log = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            configSellTime = Config.Bind("General", "sellTime", 2f, "Time in seconds to sell items (min 1 sec max 10 sec)");
+
+            if (configSellTime.Value < 1f)
+            {
+                configSellTime.Value = 1f;
+            }
+
+            if (configSellTime.Value > 10f)
+            {
+                configSellTime.Value = 10f;
+            }
+
+            log.LogInfo("Set Company Sell Time loaded");
+
+            harmony.PatchAll(typeof(DepositItemsDeskModBase));
+            harmony.PatchAll(typeof(DepositItemsDeskPatch));
+        }
+    }
+}
---
+++ Patches/DepositItemsDeskPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+
+namespace SetCompanySellTime.Patches
+{
+    [HarmonyPatch(typeof(DepositItemsDesk))]
+    internal class DepositItemsDeskPatch
+    {
+        [HarmonyPatch(nameof(DepositItemsDesk.AddObjectToDeskServerRpc))]
+        [HarmonyPostfix]
+        static void patchAddObjectToDeskServerRpc(ref float ___grabObjectsTimer)
+        {
+            ___grabObjectsTimer = DepositItemsDeskModBase.configSellTime.Value;
+        }
+
+        [HarmonyPatch(nameof(DepositItemsDesk.SetPatienceServerRpc))]
+        [HarmonyPostfix]
+        static void patchSetPatienceServerRpc(ref CompanyMood ___currentMood)
+        {
+            ___currentMood.judgementSpeed = 0f;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void patchDoorShutAfterSell(ref float ___waitingWithDoorOpenTimer)
+        {
+            ___waitingWithDoorOpenTimer = 20f;
+        }
+    }
+}
